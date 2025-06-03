61b70a4ac2c46460bc86f0ae7ec0d341de080e7f
Gonzalo Encinas <dulphyj@gmail.com>
2025-06-03 00:51:22 -0400

    edited test properties

--- frontend/saijainstore/src/environments/environment.ts
+++ frontend/saijainstore/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://20.94.243.1:8080/api',
+    apiUrl: 'http://localhost:8080/api',
     imgDefault: 'http://res.cloudinary.com/digxhcgyu/image/upload/v1743625039/default_d0ejhh.jpg'
 };
