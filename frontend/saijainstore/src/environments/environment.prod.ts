896eefec7399ddd7939f2ad4559c2ff64a299849
Gonzalo Encinas <dulphyj@gmail.com>
2025-06-06 00:44:57 -0400

    conected back

--- frontend/saijainstore/src/environments/environment.prod.ts
+++ frontend/saijainstore/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: '__BACKEND_URL__',
+  apiUrl: '/api/api',
   imgDefault: 'https://res.cloudinary.com/digxhcgyu/image/upload/v1743625039/default_d0ejhh.jpg'
 };
